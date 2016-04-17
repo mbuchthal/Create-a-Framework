7d1a2057bd7f45a3f32bbb4b6ba1408450c023d6
Mark Buchthal <mark.buchthal@gmail.com>
2016-04-17 14:38:32 -0700

    start tests and gulpfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = require(__dirname + '/lib/handle-routes');
 
 const port = process.env.PORT || 5000;
 
-const syrvup = http.createServer(router.route());
+const syrvup = module.exports = exports = http.createServer(router.route());
 
 syrvup.listen(port, () => {
   process.stdout.write('Server is running at localhost:' + port + '\n');
