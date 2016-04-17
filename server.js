ff7effb5351feafb027761ba43d527258fc59c35
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-17 10:31:09 -0700

    update router for array input

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const Router = require(__dirname + '/lib/router');
 const port = process.env.PORT || 5000;
 
 var router = new Router()
-.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Your running fine');
-  res.end();
-});
+// .get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.write('Your running fine');
+//   res.end();
+// })
+.many([{ url: '/', content: 'this is the return' },
+{ url: '/test', content: 'test content' },
+{ url: '/another', content: 'another' }
+]);
 
 const syrvup = http.createServer(router.route());
 
