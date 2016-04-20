372e5ad5174fd0977bf8da43a2fc959e7867ca42
Philip A. Hirt <phirt13@gmail.com>
2016-04-20 10:41:26 -0700

    adding PUT,PATCH,DELETE routes

--- server-example.js
+++ server-example.js
@@ -1,10 +1,16 @@
 const syrvup = require(__dirname + '/index');
 
-var testCallback = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('yay');
-      res.end();
-    };
+var testCallback = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('yay');
+  res.end();
+};
+
+var patchCallback = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('This was a test patch function');
+  res.end();
+};
 
 var sampleHtml = __dirname + '/sample.html';
 
@@ -17,6 +23,10 @@ var endpoints = {
   postUrls: [{ url: '/', saveLocation: __dirname + '/data/' }
   ],
   putUrls: [{ url: '/', saveLocation: __dirname + '/data/' }],
+  patchUrls: [
+    { url: '/', content: 'this is a return' },
+    { url: '/another', content: patchCallback }
+  ],
   deleteUrls: [{ url: '/', saveLocation: __dirname + '/data/' }]
 };
 
@@ -24,6 +34,7 @@ syrvup.port = process.env.PORT || 5000;
 syrvup.getEndpoints(endpoints.getUrls);
 syrvup.postEndpoints(endpoints.postUrls);
 syrvup.putEndpoints(endpoints.putUrls);
+syrvup.patchEndpoints(endpoints.patchUrls);
 syrvup.deleteEndpoints(endpoints.deleteUrls);
 syrvup.server.listen(syrvup.port, () => {
   process.stdout.write('Server is running at localhost:' + syrvup.port + '\n');
